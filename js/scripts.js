cf8b19cc2426ed80abf35195302b27b436c027fd
Trevor Mackin and Uriel Gonzalez <trevor@>
2019-10-24 10:04:25 -0700

    Add branching functions to animal info UI.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var animalInfo = prompt("Please choose an animal: Whale, Dog or Dolphin").toLowerCase();
+    if (animalInfo === "whale") {
+      $(".whale").show();
+    }
+    if (animalInfo === "dog") {
+      $(".dog").show();
+    }
+    if (animalInfo === "dolphin") {
+      $(".dolphin").show();
+    }
+
+    $("#whale").click(function() {
+      $("#whaleInfo").show();
+    })
+    $("#dog").click(function() {
+      $("#dogInfo").show();
+    })
+});
